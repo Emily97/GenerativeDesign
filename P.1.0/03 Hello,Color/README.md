962052cf243beb8bf786bbdbb8eabfe367b19235
Emily97 <N00150623@student.iadt.ie>
2018-10-13 15:28:47 +0100

    step three complete

---
+++ P.1.0/03 Hello,Color/README.md
@@ -0,0 +1,17 @@
+# 03 Hello, Color
+
+## Code Explained
+```js
+rectMode(CENTER);
+```
+This places the rectangle in the centre of the canvas.
+
+```js
+fill(360-mouseY/2,100,100);
+```
+The halved y-value of the mouse position is subtracted from 360, creating values from 360 to 0.
+
+```js
+rect(width/2,height/2,mouseX,mouseX);
+```
+Starts at the middle of the canvas in both the x and y direction and grows bigger and smaller depending on the mouses X position on the canvas.
--- P.1.0/03 Hello,Color/sketch.js
+++ P.1.0/03 Hello,Color/sketch.js
@@ -1,16 +1,15 @@
 function setup(){
     createCanvas(720,720);
     colorMode(HSB, 360, 100, 100);
+    //this places the rectangle in the centre of the canvas.
     rectMode(CENTER);
     noStroke();
 }
 
 function draw(){
-    background(mouseY/2,100,100); //determines the hue of the background based on the mouses Y position -> background(mouseY/2,80,80);. 
+    background(mouseY/2,100,100);
+    // The halved y-value of the mouse position is subtracted from 360, creating values from 360 to 0.
     fill(360-mouseY/2,100,100);
+    //starts at the middle of the canvas in both the x and y direction and grows bigger and smaller depending on the mouses X position on the canvas.
     rect(width/2,height/2,mouseX,mouseX);
-}
-
-function keyPressed(){
-    if(key == 's' || key == 'S') saveCanvas(gd.timestamp() + '_MouseX_' + mouseX + '_MouseY_' + mouseY,'png');
 }
