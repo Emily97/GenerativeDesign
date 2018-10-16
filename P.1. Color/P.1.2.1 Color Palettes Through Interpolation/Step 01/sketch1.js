03e1f6fcfd952a8c73488e7f0a5b5f45da4746b3
Emily97 <N00150623@student.iadt.ie>
2018-10-16 21:51:44 +0100

    do

--- P.1. Color/P.1.2.1 Color Palettes Through Interpolation/Step 01/sketch1.js
+++ P.1. Color/P.1.2.1 Color Palettes Through Interpolation/Step 01/sketch1.js
@@ -1,12 +1,8 @@
 
 'use strict';
 
-var colorLeft = [];
-var colorRight = [];
 var tileCountX = 5;
 var tileCountY = 5;
-
-
 function setup() {
     createCanvas(500, 500);
     colorMode(HSB);
@@ -17,8 +13,6 @@ function setup() {
 function draw() {
     var tileWidth = width / tileCountX;
     var tileHeight = height / tileCountY;
-
-    var interCol;
 
     for(var gridY = 0; gridY < tileCountY; gridY++) {
         for(var gridX = 0; gridX < tileCountX; gridX++) {
