01dabd55165802c4bb8d13cf46c8d544c4a06f84
Emily97 <N00150623@student.iadt.ie>
2018-10-16 14:24:04 +0100

    upload

--- P.1. Color/P.1.1.2 Color Spectrum In A Circle/Step 01/sketch1.js
+++ P.1. Color/P.1.1.2 Color Spectrum In A Circle/Step 01/sketch1.js
@@ -1,46 +1,18 @@
 'use strict';
-var segmentCount = 360;
 function setup(){
     createCanvas(800,400);
     colorMode(HSB, 360, 100, 100);
-    // rectMode(CENTER);
-    noStroke();
+    noLoop();
 }
 
 function draw(){
-  background('violet');
-  var angleIncrement = 360/segmentCount;
-  var radius = 200;
-
+  // Using the beginShape() and endShape() functions allow creating more complex forms.
   beginShape(TRIANGLE_FAN);
-  vertex(width/2,height/2);
-    for(var angle = 0; angle <= 360; angle += angleIncrement){
-      var vx = radius * cos(radians(angle)) + width/2;
-      var vy = radius * sin(radians(angle)) + height/2;
-      fill(angle, 100, 100);
-      vertex(vx,vy);
-    }
+    vertex(57.5, 50);
+    vertex(57.5, 15);
+    vertex(92, 50);
+    vertex(57.5, 85);
+    vertex(22, 50);
+    vertex(57.5, 15);
   endShape();
-}
-
-function keyPressed(){
-  if(key == 's' || key == 'S') saveCanvas(gd.timestamp(),'png');
-
-  switch (key) {
-    case '1':
-      segmentCount = 360;
-      break;
-    case '2':
-      segmentCount = 45;
-      break;
-    case '3':
-      segmentCount = 24;
-      break;
-    case '4':
-      segmentCount = 12;
-      break;
-    case '5':
-      segmentCount = 6;
-      break;
-    }
 }
