40e6cf736daaf565dbb0c85dece2ecb0c06d4af2
Emily97 <N00150623@student.iadt.ie>
2018-10-16 19:37:57 +0100

    triangle fan

--- P.1. Color/P.1.1.2 Color Spectrum In A Circle/Step 01/sketch1.js
+++ P.1. Color/P.1.1.2 Color Spectrum In A Circle/Step 01/sketch1.js
@@ -9,7 +9,7 @@ function draw(){
   // Using the beginShape() and endShape() functions allow creating more complex forms.
   // The TRIANGLE_FAN is requiredd to make a circle.
   // To start the TRIANGLE_FAN must have a starting position which is the centre and the vertexs are
-  // then made
+  // then made to create the shape. When the endShape function is declared it stops the creation of vertexes
   beginShape(TRIANGLE_FAN);
     vertex(57.5, 50);
     vertex(57.5, 15);
