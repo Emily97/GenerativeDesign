98fb459fc39de0b2cda8ef1120cfd6425b2e25a1
Emily Meagher <N00150623@iadt.ie>
2018-10-18 16:56:26 +0100

    step one

--- P.1. Color/P.1.2.3.01 Color Palettes From Rules01/Step 01/sketch.js
+++ P.1. Color/P.1.2.3.01 Color Palettes From Rules01/Step 01/sketch.js
@@ -3,21 +3,10 @@
 var tileCountX = 50;
 var tileCountY = 10;
 
-var hueValues = [];
-var saturationValues = [];
-var brightnessValues = [];
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
-  noStroke();
-
-  // init with random values
-  for (var i = 0; i < tileCountX; i++) {
-    hueValues[i] = random(360);
-    saturationValues[i] = random(100);
-    brightnessValues[i] = random(100);
-  }
+  // noStroke();
 }
 
 function draw() {
@@ -27,9 +16,6 @@ function draw() {
   // limit mouse coordinates to canvas
   var mX = constrain(mouseX, 0, width);
   var mY = constrain(mouseY, 0, height);
-
-  // tile counter
-  var counter = 0;
 
   // map mouse to grid resolution
   var currentTileCountX = int(map(mX, 0, width, 1, tileCountX));
@@ -41,12 +27,8 @@ function draw() {
     for (var gridX = 0; gridX < tileCountX; gridX++) {
       var posX = tileWidth * gridX;
       var posY = tileHeight * gridY;
-      var index = counter % currentTileCountX;
-
-      // get component color values
-      fill(hueValues[index], saturationValues[index], brightnessValues[index]);
+      fill('red');
       rect(posX, posY, tileWidth, tileHeight);
-      counter++;
     }
   }
 }
