0e5ee81a28712bdc7dfad4978e3c15b2bd6b9ab1
Emily Meagher <N00150623@iadt.ie>
2018-10-18 15:17:26 +0100

    step two complete

--- P.1. Color/P.1.2.1 Color Palettes Through Interpolation/Step 02/README.md
+++ P.1. Color/P.1.2.1 Color Palettes Through Interpolation/Step 02/README.md
@@ -1,3 +1,30 @@
 # 02 Color Palettes Through Interpolation
 
 ## Code Explained
+```js
+var colorLeft = [];
+var colorRight = [];
+```
+Declare two arrays that will fill the left and right most columns with colors, though currently they will be empty.
+
+```js
+var startColor = colorLeft[gridY];
+var endColor = colorRight[gridY];
+```
+These variables store the colors for the left and right columns
+
+```js
+var amount = map(gridX, 0, 10, 0, 1);
+var interCol = lerpColor(startColor, endColor, amount);
+fill(interCol);
+```
+The colors found in-between the leftmost columns and the rightmost column are calculated with lerpColor(). lerpColor() performs the Interpolation between the two values startColor and endColor. Amount specifies the startColor and endColor position. In this case the first color will be 0.0 whereas the fifth color will be 0.5.
+
+```js
+function shakeColor() {
+	for (var i = 0; i < numberOfRows; i++){
+		colorLeft[i] = color(random(255), random(255), random(255)); //generating 10 random colours
+		colorRight[i] = color(random(255), random(255), random(255)); //generating 10 random colours
+	}
+``
+ShakeColors is a function that is declared in the setup. This function creates a random color for the start and end of each row which is then interpolated by lerpColor to interpolate the other colours in-between.
--- P.1. Color/P.1.2.1 Color Palettes Through Interpolation/Step 02/sketch1.js
+++ P.1. Color/P.1.2.1 Color Palettes Through Interpolation/Step 02/sketch1.js
@@ -1,14 +1,10 @@
 'use strict';
-
+//declare two arrays that will fill the left and right most columns with colors, though currently they will be empty
 var colorLeft = [];
 var colorRight = [];
 
-//var myColor;
-//var randomColor;
-
 var numberOfRows = 10;
 var numberOfColumns = 10;
-
 var tileWidth;
 var tileHeight;
 
@@ -20,20 +16,21 @@ function setup() {
 	noStroke();
 	shakeColor();
 
-	tileWidth = width/numberOfColumns;
-	tileHeight = height/numberOfRows;
-
 }
 
 function draw(){
+  tileWidth = width/numberOfColumns;
+  tileHeight = height/numberOfRows;
 
 	for(var gridY = 0; gridY < numberOfColumns; gridY++){
+    //these variables store the colors for the left and right columns
+    var startColor = colorLeft[gridY];
+    var endColor = colorRight[gridY];
 		for(var gridX = 0; gridX < numberOfRows; gridX++){
-
+      //the colors found in-between the leftmost columns and the rightmost column are calculated with lerpColor()
+      //lerpColor() performs the Interpolation between the two values startColor and endColor
+      //amount specifies the startColor and endColor position in this case the first color will be 0.0 whereas the fifth color will be 0.5
 			var amount = map(gridX, 0, 10, 0, 1);
-			var startColor = colorLeft[gridY];
-			var endColor = colorRight[gridY];
-
 			var interCol = lerpColor(startColor, endColor, amount);
 			fill(interCol);
 
@@ -43,12 +40,12 @@ function draw(){
 
 
 }
-
+//shakeColors is a function that is declared in the setup
+// this function creates a random color for the start and end of each row which is then interpolated by lerpColor to interpolate the other colours in-between.
 function shakeColor() {
-
 	for (var i = 0; i < numberOfRows; i++){
-		colorLeft[i] = color(random(255), random(255), random(255)); //generating 5 random colours
-		colorRight[i] = color(random(255), random(255), random(255)); //generating 5 random colours
+		colorLeft[i] = color(random(255), random(255), random(255)); //generating 10 random colours
+		colorRight[i] = color(random(255), random(255), random(255)); //generating 10 random colours
 	}
 
 }
