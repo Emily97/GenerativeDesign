b3c3ba63316a9ec36696c4fbcaa79e0766edf636
Emily97 <N00150623@student.iadt.ie>
2018-10-19 16:23:52 +0100

    steps complete

--- P.1. Color/P.1.2.3.01 Color Palettes From Rules01/Step 01/sketch.js
+++ P.1. Color/P.1.2.3.01 Color Palettes From Rules01/Step 01/sketch.js
@@ -10,18 +10,10 @@ function setup() {
 }
 
 function draw() {
-  // white back
   background(0, 0, 100);
 
-  // limit mouse coordinates to canvas
-  var mX = constrain(mouseX, 0, width);
-  var mY = constrain(mouseY, 0, height);
-
-  // map mouse to grid resolution
-  var currentTileCountX = int(map(mX, 0, width, 1, tileCountX));
-  var currentTileCountY = int(map(mY, 0, height, 1, tileCountY));
-  var tileWidth = width / currentTileCountX;
-  var tileHeight = height / currentTileCountY;
+  var tileWidth = width / tileCountX;
+  var tileHeight = height / tileCountY;
 
   for (var gridY = 0; gridY < tileCountY; gridY++) {
     for (var gridX = 0; gridX < tileCountX; gridX++) {
