3da44aa7d6c832a0f4fadab04deaa82d16a258ef
Emily97 <N00150623@student.iadt.ie>
2018-10-29 16:12:12 +0000

    commit file changes

--- P.1. Color/P.1.2.3.03 Color Palettes From Rules03/Step 03/sketch.js
+++ P.1. Color/P.1.2.3.03 Color Palettes From Rules03/Step 03/sketch.js
@@ -4,54 +4,44 @@ var colorCount = 20;
 var hueValues = [];
 var saturationValues = [];
 var brightnessValues = [];
-var actRandomSeed = 0;
-var alphaValue = 27;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360, 100, 100, 100);
+  colorMode(HSB, 360, 100, 100);
   noStroke();
 }
 
 function draw() {
   noLoop();
-  background(0);
-  randomSeed(actRandomSeed);
-
-  // ------ colors ------
-  // create palette
   for (var i = 0; i < colorCount; i++) {
     if (i % 2 == 0) {
-      hueValues[i] = random(360);
+      hueValues[i] = random(220, 290);
       saturationValues[i] = 100;
-      brightnessValues[i] = random(100);
+      brightnessValues[i] = random(15, 100);
     } else {
-      hueValues[i] = 195;
-      saturationValues[i] = random(100);
+      hueValues[i] = 260;
+      saturationValues[i] = random(20, 100);
       brightnessValues[i] = 100;
     }
   }
-
-  // ------ area tiling ------
-  // count tiles
   var counter = 0;
-  // row count and row height
-  var rowCount = int(random(5, 30));
-  var rowHeight = height / rowCount;
+  // tile count picks an integer value between 5 and 30 to determine how many rows are created
+  // tile height determines the height of the tiles depending on the number of rows
+  var tileCountY = int(random(5,30));
+  var tileHeight = height / tileCountY;
 
-  // seperate each line in parts
-  for (var i = rowCount; i >= 0; i--) {
-    // how many fragments
-    var partCount = i + 1;
+  // seperate each row in parts
+  for (var gridY = tileCountY; gridY >= 0; gridY--) {
+    // how many rectangles in each row depends on the tileCountY value plus 1.
+    var numRect = gridY + 1;
     var parts = [];
 
-    for (var ii = 0; ii < partCount; ii++) {
-      // sub fragments or not?
+    // if random is less than 0.05 create fragments to fill up the parts array
+    for (var i = 0; i < numRect; i++) {
       if (random() < 0.075) {
-        // take care of big values
         var fragments = int(random(2, 20));
-        partCount = partCount + fragments;
-        for (var iii = 0; iii < fragments; iii++) {
+        numRect += fragments;
+        for (var ii = 0; ii < fragments; ii++) {
           parts.push(random(2));
         }
       } else {
@@ -59,54 +49,27 @@ function draw() {
       }
     }
 
-    // add all subparts
-    var sumPartsTotal = 0;
-    for (var ii = 0; ii < partCount; ii++) {
-      sumPartsTotal += parts[ii];
+    // add all the parts from the parts array together to get the total
+    var totalParts = 0;
+    for (var j = 0; j < numRect; j++) {
+      totalParts += parts[j];
     }
 
-    // draw rects
-    var sumPartsNow = 0;
-    for (var ii = 0; ii < parts.length; ii++) {
-      sumPartsNow += parts[ii];
+    // draw rects for each row depending on the value of the parts array
+    var currentParts = 0;
+    for (var gridX = 0; gridX < parts.length; gridX++) {
 
-      var x = map(sumPartsNow, 0, sumPartsTotal, 0, width);
-      var y = rowHeight * i;
-      var w = -map(parts[ii], 0, sumPartsTotal, 0, width);
-      var h = rowHeight * 1.5;
-
+      var posX = map(currentParts, 0, totalParts, 0, width);
+      var posY = tileHeight * gridY;
+      // the width of the rectangle goes in the negative direction from the posX
+      // it doesn't exceed the width of the canvas
+      var w = -map(parts[gridX], 0, totalParts, 0, width);
       var index = counter % colorCount;
-      var col1 = color(0);
-      var col2 = color(hueValues[index], saturationValues[index], brightnessValues[index], alphaValue);
-      gradient(x, y, w, h, col1, col2);
-
+      var col = color(hueValues[index], saturationValues[index], brightnessValues[index]);
+      fill(col);
+      rect(posX, posY, w, tileHeight);
+      currentParts += parts[gridX];
       counter++;
     }
-  }
-}
-
-function gradient(x, y, w, h, c1, c2) {
-  var ctx = drawingContext; // global canvas context p5.js var
-  var grd = ctx.createLinearGradient(x, y, x, y + h);
-  grd.addColorStop(0, c1.toString());
-  grd.addColorStop(1, c2.toString());
-	ctx.fillStyle = grd;
-	ctx.fillRect(x, y, w, h);
-}
-
-function mouseReleased() {
-  actRandomSeed = random(100000);
-  loop();
-}
-
-function keyPressed() {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  if (key == 'c' || key == 'C') {
-    // -- save an ase file (adobe swatch export) --
-    var colors = [];
-    for (var i = 0; i < hueValues.length; i++) {
-      colors.push(color(hueValues[i], saturationValues[i], brightnessValues[i]));
-    }
-    writeFile([gd.ase.encode(colors)], gd.timestamp(), 'ase');
   }
 }
