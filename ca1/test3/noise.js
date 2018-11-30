c316d34ab9af1bfa42102e107765d6e1ebf86e49
Emily97 <N00150623@student.iadt.ie>
2018-11-30 16:10:12 +0000

    update comments

--- ca1/test3/noise.js
+++ ca1/test3/noise.js
@@ -1,58 +1,51 @@
-function Noise(xx, yy, minSize, maxSize, c) {
-  this.segments = 50;
-  this.numberOfAngles = TWO_PI/this.segments;
-  this.noiseLevel = 0.5;
-  this.time = 0.01;
-  this.timeDifference = 1000;
+//review nature of code forces chapter
+//constructor
+function Noise(x, y, minSize, maxSize, c) {
+  this.noOfPoints = floor(random(3,50)); //the number of points to make shapes 3 points make a triangle
+  this.angleSize = TWO_PI/this.noOfPoints; //the size of the angle is determined by the number of points for the shape
+  this.noiseLevel = 0.5; //noise level between 0.0 and 1.0
+  this.time = 0.01; //determines how fast the noise level changes
+  this.timeDifference = 10000;
 
-  this.dx = 0;
-  this.dy = 0;
+  this.dx = 0; //start x position
+  this.dy = 0; //start y position
 
-  this.randomTime = random(this.timeDifference);
-  this.x = xx;
-  this.y = yy;
-  this.minRadius = minSize;
-  this.maxRadius = maxSize;
-  this.hue = c;
+  this.randomTime = random(this.timeDifference); //a random time is picked between 0 and timeDifference value
+  this.x = x; //the x coordinate
+  this.y = y; //the y coordinate
+  this.minRadius = minSize; //the minRadius size
+  this.maxRadius = maxSize; //the maxRadius size
+  this.hue = c; //the hue value
 
-  this.create = function() {
-    translate(this.x, this.y);
-    fill(this.hue%360, 100, 100, 100);
+  this.show = function() {
+    translate(this.x, this.y); //places the shapes where the x and y position states
+    fill(this.hue%360, 100, 100, 100); //if the hue value exceeds 360 the hue will be reset to 1 due to the modular which gets the remainder
 
-    beginShape();
-    this.findNextCoordinates(0);
-    var px = this.dx;
-    var py = this.dy;
+    beginShape(); //the beginShape and endShape allows the creation of complex shapes
     var i = 0;
-
-    while (i++ != this.segments) {
-      this.findNextCoordinates(i);
-
-      vertex(px, py);
-      px = this.dx;
-      py = this.dy;
+    while (i++ != this.noOfPoints) { //if the incremented i is not equal to the noOfPoints then execute the following
+      this.follow(i); //follows the trail made by the noise level changing the edge of the shape
+      vertex(px, py); //creates the number of vertex's until the i increment is equal to noOfPoints
+      var px = this.dx; //the starting x position
+      var py = this.dy; //the starting y position
     }
 
-    endShape(CLOSE);
-
-    resetMatrix();
-
-    this.hue += 0.1;
+    endShape(CLOSE); //the constant CLOSE as the value for the MODE parameter to close the shape (to connect the beginning and the end)
+    resetMatrix(); //replaces the current matrix so the shapes don't translate by the x and y position each time the shape is made
+    this.hue += 0.1; //increments the hue value each frame
   };
 
-  this.findNextCoordinates = function(seg) {
-    var angle = this.numberOfAngles*seg;
-    var cosAngle = cos(angle);
-    var sinAngle = sin(angle);
-    var time = this.time*frameCount + this.randomTime;
+  this.follow = function(points) { //vectors create a trail to follow the current vector
+    var angle = this.angleSize*points;  //the size of the angle is determined by the number of points for the shape
+    var cosAngle = cos(angle); //calculates the cos of the angle
+    var sinAngle = sin(angle); //calculates the sin of the angle
+    var time = this.time*frameCount + this.randomTime; //calculating how fast the noise level increases and decreases the size of the shape
 
-    var noiseValue = noise(
-      this.noiseLevel*cosAngle + this.noiseLevel,
-      this.noiseLevel*sinAngle + this.noiseLevel, time);
+    //how often the noise value changes over time that changes the size of the shape
+    var noiseValue = noise(this.noiseLevel*cosAngle + this.noiseLevel, this.noiseLevel*sinAngle + this.noiseLevel, time);
 
-    var rad = this.maxRadius*noiseValue + this.minRadius;
-
-    this.dx = rad*cosAngle;
-    this.dy = rad*sinAngle;
+    var radius = this.maxRadius*noiseValue + this.minRadius;
+    this.dx = radius*cosAngle; //the x position to draw the vertexs'
+    this.dy = radius*sinAngle; //the y position to draw the vertexs'
   };
 }
--- ca1/test3/sketch.js
+++ ca1/test3/sketch.js
@@ -1,30 +1,31 @@
-var numberOfCircles = 7;
-var circles = [];
+var numberOfShapes = 7; //the number of shapes to be created
+var shapes = []; //empty array for shapes
 
-var framesPerSecond = 40;
+var framesPerSecond = 50; //the framerate affects the spped at which the shapes change shape due to noise
 
 function setup() {
-  createCanvas(1240,1748);
-  colorMode(HSB, 360, 100, 100, 50);
-  frameRate(framesPerSecond);
-  ellipseMode(CENTER);
-  noStroke();
+  createCanvas(1240,1748); //A4 canvas for book cover
+  colorMode(HSB, 360, 100, 100, 50); //HUE, SATURATION, BRIGHTNESS, ALPHA
+  frameRate(framesPerSecond); //the frame rate is declared above
+  noStroke(); //no stroke on the shapes
 
-  var centreX = width/2;
-  var centreY = height/2;
+  var x = width/2; //the centre of the shape x coordinate
+  var y = height/2; //the centre of the shape y coordinate
+  var min = width/6; //the smallest size the shape can be
+  var max = height/4; //the largest size the shape can be
 
-  for (var i = 0; i < numberOfCircles; i++) {
-    circles.push(new Noise(centreX, centreY, width/6, height/4, 360*i/numberOfCircles));
+  for (var i = 0; i < numberOfShapes; i++) { //create the number of shapes specified and pushed into the shapes array
+    shapes.push(new Noise(x, y, min, max, 360*i/numberOfShapes)); //pushes the shapes into the shapes array declared above
   }
 }
 
 function draw() {
-  blendMode(BLEND);
-  background(255);
-  blendMode(DIFFERENCE);
+  blendMode(BLEND); //linear interpolation of colours
+  background(255); //refreshes the background every frame
+  blendMode(DIFFERENCE); //subtract colors from underlying image
 
-  for (var i = 0; i < numberOfCircles; i++) {
-    circles[i].create();
+  for (var i = 0; i < numberOfShapes; i++) { //the noise class takes whats declared in the show function
+    shapes[i].show();                        //and puts it onto the number of shapes created
   }
 }
 
