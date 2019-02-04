73bc2196d46de8930f21315c6a87be0877975eeb
Emily Meagher <N00150623@iadt.ie>
2019-02-04 13:53:44 +0000

    update

---
+++ type/type_from_outline_template/sketch.js
@@ -0,0 +1,36 @@
+let font;
+let textTyped = "O";
+let path;
+function setup(){
+  createCanvas(500,500);
+  opentype.load('data/FreeSans.otf',function(err,f){
+    if(err){
+      console.log(err);
+    }
+    else{
+      font = f;
+      noLoop()
+    }
+  });
+}
+function draw(){
+  if(!font) return;
+  background(255);
+  translate(20,220);
+
+  if(textTyped.length > 0){
+    let fontPath = font.getPath(textTyped,0,0,200);
+    console.log(fontPath);
+    path = new g.Path(fontPath.commands);
+    path = g.resampleByLength(path,10);
+    console.log(path);
+  }
+  fill(0);
+  noStroke();
+  let diameter = 5;
+
+  for(let i = 0; i < path.commands.length;i++){
+    let pnt = path.commands[i];
+    ellipse(pnt.x,pnt.y,diameter,diameter);
+  }
+}
---
+++ type/type_from_pixels_template/sketch.js
@@ -0,0 +1,36 @@
+let font;
+let textImg;
+let numOfTiles = 200;
+let tileWidth;
+function preload(){
+  font = loadFont('data/FreeSansBold.ttf');
+}
+function setup(){
+  background(255);
+  createCanvas(500,500);
+  tileWidth = floor(width/numOfTiles);
+  setupText();
+}
+function draw(){
+  // stroke(255);
+  fill('grey');
+  for(let y = 0; y < width; y+=tileWidth){
+    for(let x = 0; x < width;x+=tileWidth){
+      let index = (x + y * textImg.width) * 4;
+      if(textImg.pixels[index] < 128){
+        rect(x,y,tileWidth,tileWidth);
+      }
+
+    }
+  }
+}
+
+function setupText(){
+  textImg = createGraphics(500,500);
+  textImg.pixelDensity(1);
+  textImg.background(255);
+  textImg.textFont(font);
+  textImg.textSize(100);
+  textImg.text("ABC",0,100,50,50);
+  textImg.loadPixels();
+}
